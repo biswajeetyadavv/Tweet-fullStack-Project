67fd762acf7834feaec3a223bf210f7d2d0d1e95
Biswajeet yadav <biswajeetyadavofficial@gmail.com>
2025-10-05 17:31:50 +0530

    feature add delete and cancel

--- tweetfullstack/tweet/views.py
+++ tweetfullstack/tweet/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Tweet
 from .forms import TweetForm
 from django.shortcuts import get_object_or_404, redirect
+from django
 
 # Create your views here.
 def index(request):
